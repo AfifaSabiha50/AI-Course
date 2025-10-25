2480f433d64281ccb4bb717f41b609b97aa63ae3
AfifaSabiha50 <160310440+AfifaSabiha50@users.noreply.github.com>
2025-10-25 05:53:18 -0700

    Add minimax function with user input for scores
    Implement minimax algorithm for optimal decision making with user input for leaf node scores.

--- Algorithm Implementation/minimax.py
+++ Algorithm Implementation/minimax.py
@@ -1,1 +1,23 @@
+def minimax(depth, node_index, is_max, scores, max_depth):
+    if depth == max_depth:
+        return scores[node_index]
+
+    if is_max:
+        left = minimax(depth+1, node_index*2, False, scores, max_depth)
+        right = minimax(depth+1, node_index*2+1, False, scores, max_depth)
+        return max(left, right)
+    else:
+        left = minimax(depth+1, node_index*2, True, scores, max_depth)
+        right = minimax(depth+1, node_index*2+1, True, scores, max_depth)
+        return min(left, right)
+
+
+
+scores = input("Enter leaf node scores: ").split()
+scores = [int(x) for x in scores]
+
+import math
+max_depth = int(math.log(len(scores), 2))
+
+print("Optimal value (Minimax):", minimax(0, 0, True, scores, max_depth))
 
